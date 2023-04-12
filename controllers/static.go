2eb5c4fba0a7f931cc8874577c5f3314f45c9d96
SHREYAS-KS-CS104 <shreyasks.cs18@bmsce.ac.in>
2023-04-12 19:54:51 +0530

    Made the FAQ page use dynamically generated data

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/SHREYAS-KS-CS104/shotDetect/views"
@@ -9,5 +10,32 @@ import (
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "What is your name?",
+			Answer:   "Yes!",
+		},
+		{
+			Question: "Where are you from?",
+			Answer:   "Yes!",
+		},
+		{
+			Question: "What do you do?",
+			Answer:   "Yes!",
+		},
+		{
+			Question: "What is What?",
+			Answer:   "Yes!",
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
