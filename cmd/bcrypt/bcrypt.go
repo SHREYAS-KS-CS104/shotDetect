82f75cd02d612db86d9a129bb798c5d118a8c1c2
SHREYAS-KS-CS104 <shreyasks.cs18@bmsce.ac.in>
2023-04-14 18:05:02 +0530

    .

--- cmd/bcrypt/bcrypt.go
+++ cmd/bcrypt/bcrypt.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
@@ -11,13 +13,25 @@ func main() {
 		hash(os.Args[2])
 	case "compare":
 		compare(os.Args[2], os.Args[3])
+	default:
+		fmt.Printf("Invalid command: %v\n", os.Args[1])
 	}
 }
 
 func hash(password string) {
-	fmt.Printf("TODO Hash the password %q\n", password)
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Printf("error hashing: %v\n", password)
+		return
+	}
+	fmt.Printf(string(hashedBytes))
 }
 
 func compare(password, hash string) {
-	fmt.Printf("TODO: Compare the password %q with the hash %q\n", password, hash)
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		fmt.Printf("Password is invalid %v\n", password)
+		return
+	}
+	fmt.Println("Password is correct!")
 }
--- go.mod
+++ go.mod
@@ -23,12 +23,12 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/rjeczalik/notify v0.0.0-20181126183243-629144ba06a1 // indirect
-	golang.org/x/crypto v0.6.0 // indirect
-	golang.org/x/net v0.6.0 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/term v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/term v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
 	mvdan.cc/sh/v3 v3.6.0 // indirect
 )
