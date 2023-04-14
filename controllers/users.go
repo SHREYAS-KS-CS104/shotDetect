2b48d85d8d04b7bf4eb964776bcf65fe430938f7
SHREYAS-KS-CS104 <shreyasks.cs18@bmsce.ac.in>
2023-04-15 01:16:32 +0530

    Created the route to Validate users

--- controllers/users.go
+++ controllers/users.go
@@ -42,3 +42,19 @@ func (u Users) LogIn(w http.ResponseWriter, r *http.Request) {
 	data.Email = r.FormValue("email")
 	u.Templates.LogIn.Execute(w, data)
 }
+
+func (u Users) ProcessLogIn(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Email    string
+		Password string
+	}
+	data.Email = r.FormValue("email")
+	data.Password = r.FormValue("password")
+	user, err := u.UserService.Authenticate(data.Email, data.Password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Invalid Log in", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "User authenticated: %+v", user)
+}
--- main.go
+++ main.go
@@ -59,6 +59,7 @@ func main() {
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
 	r.Get("/login", usersC.LogIn)
+	r.Post("/login", usersC.ProcessLogIn)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
