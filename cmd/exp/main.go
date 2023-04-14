2e79780b0b9f6146259abe38f7e51efa11524651
SHREYAS-KS-CS104 <shreyasks.cs18@bmsce.ac.in>
2023-04-14 17:28:02 +0530

    .

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -78,29 +78,61 @@ func main() {
 		}
 		fmt.Println("User created! id =", id)
 	*/
-
-	id := 1
-	row := db.QueryRow(`
-		SELECT name, email
-		FROM users
-		WHERE id=$1;`, id)
-	var name, email string
-	err = row.Scan(&name, &email)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("User information: name=%s, email=%s\n", name, email)
-
-	userID := 1
-	for i := 1; i <= 5; i++ {
-		amount := i * 100
-		desc := fmt.Sprintf("Fake order #%d", i)
-		_, err := db.Exec(`
-			INSERT INTO orders(user_id, amount, description)
-			VALUES($1,$2,$3)`, userID, amount, desc)
+	/*
+		id := 1
+		row := db.QueryRow(`
+			SELECT name, email
+			FROM users
+			WHERE id=$1;`, id)
+		var name, email string
+		err = row.Scan(&name, &email)
 		if err != nil {
 			panic(err)
 		}
+		fmt.Printf("User information: name=%s, email=%s\n", name, email)
+
+		userID := 1
+		for i := 1; i <= 5; i++ {
+			amount := i * 100
+			desc := fmt.Sprintf("Fake order #%d", i)
+			_, err := db.Exec(`
+				INSERT INTO orders(user_id, amount, description)
+				VALUES($1,$2,$3)`, userID, amount, desc)
+			if err != nil {
+				panic(err)
+			}
+		}
+		fmt.Println("Created fake orders")
+	*/
+	type Order struct {
+		ID          int
+		UserID      int
+		Amount      int
+		Description string
 	}
-	fmt.Println("Created fake orders")
+	var orders []Order
+	userID := 1
+	rows, err := db.Query(`
+		SELECT id, amount, description
+		FROM orders
+		WHERE user_id=$1`, userID)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var order Order
+		order.UserID = userID
+		err := rows.Scan(&order.ID, &order.Amount, &order.Description)
+		if err != nil {
+			panic(err)
+		}
+		orders = append(orders, order)
+	}
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Orders:", orders)
 }
