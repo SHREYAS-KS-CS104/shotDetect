685b09d2f5839c368d2d099ea6789f914576cd0e
SHREYAS-KS-CS104 <shreyasks.cs18@bmsce.ac.in>
2023-04-20 22:56:52 +0530

    Added the update password function

--- controllers/users.go
+++ controllers/users.go
@@ -163,7 +163,11 @@ func (u Users) ProcessResetPassword(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Something went wrong", http.StatusInternalServerError)
 	}
 
-	//TODO Update the user's password.
+	err = u.UserService.UpdatePassword(user.ID, data.Password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+	}
 
 	// Sign the user in now that their password has been reset.
 	// Any errors from this point onwards should redirect the user  to the sign in
